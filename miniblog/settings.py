6975918810a779239392366614358ef47f52c204
Jacopo Corzani <corzani@gmail.com>
2013-04-03 01:21:16 +0100

    ADDED - Extern CSS file

--- miniblog/settings.py
+++ miniblog/settings.py
@@ -1,3 +1,4 @@
+
 # Django settings for miniblog project.
 
 DEBUG = True
@@ -51,6 +52,7 @@ USE_TZ = True
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
 MEDIA_ROOT = ''
+
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
